049ed4864dd9b13d73b3358c644659d620bafbde
lukmanpratama~ <zoezta01@gmail.com>
2023-09-27 20:08:12 +0700

    foreign

--- database/migrations/2023_09_27_051049_create_managers_table.php
+++ database/migrations/2023_09_27_051049_create_managers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('managers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
