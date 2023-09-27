fb8148c05ce4c3e58fc44181f941905b746b8b02
lukmanpratama~ <zoezta01@gmail.com>
2023-09-27 20:05:54 +0700

    user_id

--- database/migrations/2023_09_27_051037_create_owners_table.php
+++ database/migrations/2023_09_27_051037_create_owners_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('owners', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
